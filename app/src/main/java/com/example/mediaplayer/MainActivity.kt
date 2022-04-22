65bd0abd11d57e05c5c318eab1dcb54854f1e35a
zahhard <za.darardoust@gmail.com>
2022-04-23 02:25:10 +0430

    mediaRecorder added

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,25 +1,88 @@
 package com.example.mediaplayer
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.net.wifi.WifiManager
 import android.net.wifi.WifiManager.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PowerManager
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class MainActivity : AppCompatActivity() {
-    var mediaPlayer = MediaPlayer()
-    var isPause = true
+    private var mediaPlayer = MediaPlayer()
+    private var mediaRecorder = MediaRecorder()
+    private var isPause = true
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var fileName: String = ""
+    private var isRecord = false
+    private var i = 0
+    private var j = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btnPlay = findViewById<Button>(R.id.btn_play)
-        var btnPause = findViewById<Button>(R.id.btn_puse)
-        initMediaPlayer(this)
 
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnPause = findViewById<Button>(R.id.btn_puse)
+        val btnRecord = findViewById<Button>(R.id.btn_record)
+        val btnPlayMyVoice = findViewById<Button>(R.id.btn_play_voice)
+
+        val temp = "zahraTestRecordingVoice"
+        fileName = "${externalCacheDir?.absolutePath}/$temp.3gp"
+
+        givePermission()
+        initMediaPlayer()
+        initRecordeer()
+        mediaPlayerButtonsHandler(btnPlay, btnPause)
+
+        btnRecord.setOnClickListener {
+           /// mediaRecorder.start()
+             i ++
+            isRecord = i % 2 != 0
+            onRecord(isRecord)
+        }
+
+        btnPlayMyVoice.setOnClickListener {
+            j++
+            isRecord = j % 2 != 0
+            onPlay(isRecord)
+        }
+    }
+
+    private fun givePermission() {
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initRecordeer() {
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+
+            }
+        }
+    }
+
+    private fun mediaPlayerButtonsHandler(
+        btnPlay: Button,
+        btnPause: Button
+    ) {
         btnPlay.setOnClickListener {
             mediaPlayer.start()
             isPause = false
@@ -30,12 +93,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initMediaPlayer(context: Context) {
+    private fun initMediaPlayer() {
 
-        //  val myUrl = "https://dls.music-fa.com/tagdl/downloads/Salar%20Aghili%20-%20Baradar%20Jan%20(320).mp3" // initialize Uri here
-
-
-        val url = "https://dls.music-fa.com/tagdl/downloads/Salar%20Aghili%20-%20Baradar%20Jan%20(320).mp3" // your URL here
+        val url =
+            "https://dls.music-fa.com/tagdl/downloads/Salar%20Aghili%20-%20Baradar%20Jan%20(320).mp3" // your URL here
         mediaPlayer = MediaPlayer().apply {
             setAudioAttributes(
                 AudioAttributes.Builder()
@@ -47,11 +108,11 @@ class MainActivity : AppCompatActivity() {
             prepare() // might take long! (for buffering, etc)
         }
         setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
-        //mediaPlayer.setOnErrorListener(this)
     }
 
     private fun setWakeMode(applicationContext: Context?, partialWakeLock: Int) {
-        val wifiManager = getApplicationContext()?.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiManager =
+            getApplicationContext()?.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val wifiLock: WifiLock =
             wifiManager.createWifiLock(WIFI_MODE_FULL, "mylock")
 
@@ -63,11 +124,73 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-
         mediaPlayer.release()
     }
 
-    //mediaPlayer = MediaPlayer.create(context, R.raw.sound_file_1)
-    //mediaPlayer.start() // no need to call prepare(); create() does that for you
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
+    }
+///////////////////////////////////////////////////////////////////////////
 
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
+    }
+
+    private fun startPlaying() {
+        mediaPlayer = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                //Log.e(LOG_TAG, "prepare() failed")
+            }
+        }
+    }
+
+    private fun stopPlaying() {
+        mediaPlayer.release()
+    }
+
+    private fun startRecording() {
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+               // Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
+        }
+    }
+
+    private fun stopRecording() {
+        mediaRecorder.apply {
+            stop()
+            release()
+        }
+    }
 }
